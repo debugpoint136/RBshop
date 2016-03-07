518180bda5eca21a1dbe8f34682cdee69c6c0ed6
Deepak Purushotham <dpurushotham136@gmail.com>
2016-03-07 17:23:07 -0600

    starting pooling together of Data Sources

--- app/client/templates/select_dataset/select_dataset.js
+++ app/client/templates/select_dataset/select_dataset.js
@@ -1,3 +1,18 @@
+/*****GLOBAL VARIABLES*****/
+
+geo2id = {};
+id2geo = {};
+geoFlatENCODE = {};
+geoFlatROADMAP = {};
+
+geoURLencode = 'http://vizhub.wustl.edu/public/hg19/encode.md';
+geoURLroadmap = 'http://vizhub.wustl.edu/public/hg19/roadmap9_methylC.md';
+
+expURLAssays = 'http://vizhub.wustl.edu/metadata/Experimental_assays';
+donorURL     = 'http://vizhub.wustl.edu/metadata/human/Donor';
+samplesURL   = 'http://vizhub.wustl.edu/metadata/human/Samples';
+institutionsURL = 'http://vizhub.wustl.edu/metadata/Institutions';
+
 /*****************************************************************************/
 /* select_dataset: Event Handlers */
 /*****************************************************************************/
@@ -18,19 +33,33 @@ Template.select_dataset.events({
 /* select_dataset: Helpers */
 /*****************************************************************************/
 Template.select_dataset.helpers({
-
+    datapageReady: function () { 
+        return Session.get('isDataGridReady')
+    }
 });
 
 /*****************************************************************************/
 /* select_dataset: Lifecycle Hooks */
 /*****************************************************************************/
 Template.select_dataset.onCreated(function () {
+    Session.set('isDataGridReady', false);
+    
+    callAPIforGEO();
+    setTimeout( callAPI('httpGETcall', geoURLencode, geoFlatENCODE ), 4000);
+    callAPI('httpGETcall', geoURLroadmap,  geoFlatROADMAP );
+
+    setTimeout(doneLoading, 4000);
 });
 
 Template.select_dataset.onRendered(function () {
+
+
+    // Load completed
+    
 });
 
 Template.select_dataset.onDestroyed(function () {
+    Session.set('isDataGridReady', false);
 });
 
 /*****************************************************************************/
@@ -43,4 +72,76 @@ function range(start, end) {
         foo.push(i.toString());
     }
     return foo;
+}
+
+
+function doneLoading(){
+  Session.set('isDataGridReady', true);
+}
+
+
+/*==============Fetch globally required data=========*/
+
+function callAPIforGEO() {
+    Meteor.call('httpGETcall', function(error, res) {
+      if (!error) {
+            var data = eval('(' + res + ')');
+            bootstrapGEO(data);
+      } else {
+        console.log(error);
+      }
+    });
+}
+
+function bootstrapGEO(data) {
+    /* entire set of geo */
+    for(i = 0; i < data.geo.length; i++ ) {
+        var v = data.geo[i];
+        // geo, id, label, treatFiles, inputFiles
+        geo2id[v[0]] = v[1];
+        id2geo[v[1]] = {
+            acc:        v[0],
+            label:      v[2],
+            treatment:  v[3].split(','),
+            input:(v[4].length == 0 ? null : v[4].split(','))
+        };
+    }
+}
+
+
+function callAPI(apiName, url, postProcess) {
+    Meteor.call(apiName, url, function(error, res) {
+      if (!error) {
+            var data = eval('(' + res + ')');
+            cleanUpGEO(data, postProcess);
+      } else {
+        console.log(error);
+      }
+    });
+}
+
+
+function cleanUpGEO(res, geoFlat) {
+    res.forEach(function(d) {
+        if (d.geo) {
+          var geoName = d.geo[0];
+          // Re-format the objects 
+            var obj = {};
+
+            obj['name'] = d.name;
+            obj['detail_url'] = d.detail_url;
+            obj['metadata'] = d.metadata;
+            obj['type'] = d.type;
+            obj['public'] = d.public;
+            obj['url'] = d.url;
+
+            // var metadata = d.name.split(' ');
+
+            if (geoFlat[geoName]) {
+              geoFlat[geoName].push(obj);
+            } else {
+              geoFlat[geoName] = [obj];
+            }
+        }   
+    });
 }
--- app/lib/methods.js
+++ app/lib/methods.js
@@ -50,6 +50,25 @@ if (Meteor.isServer) {
             } else {
                 throw new Meteor.Error('HTTP get status ' + result.statusCode);
             }
+        },
+
+        'httpGETcall': function (urlString) {
+            this.unblock();
+            if (! urlString ) {
+                var urlString = 'http://epigenomegateway.wustl.edu/cgi-bin/subtleKnife?loadgenome%3Don%26dbName%3Dhg19%26rpbrDbname%3Dhg19repeat%26rpbr_init%3Don&session=ZDFE9bTE2e&statusId=1&hmspan=1100';
+            }
+            
+            try {
+                var result = Meteor.http.call('GET', urlString);
+            } catch (error) {
+                throw new Meteor.Error(error.getMessage());
+            }
+
+            if (result.statusCode === 200) {
+                return result.content;
+            } else {
+                throw new Meteor.Error('HTTP get status ' + result.statusCode);
+            }
         }
     });
 }
